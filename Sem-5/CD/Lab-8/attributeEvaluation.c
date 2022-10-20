019afb2064483cdf8ebd3be517e42df726ac4c31
Nagasai Vegur <nsvegur@gmail.com>
2022-10-20 07:45:26 +0530

    :heavy_plus_sign: attribute evaluation code is added

---
+++ Sem-5/CD/Lab-8/attributeEvaluation.c
@@ -0,0 +1,197 @@
+// Roll Number: CS20B1016
+// Name: V Nagasai
+
+// Given a real number in bit string representation as input,
+// write a program to evaluate it according to the attribute evaluation strategy and
+// give the output as a real number in decimal representation.
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct parsetree
+{
+    char c;
+    struct parsetree *left;
+    struct parsetree *right;
+};
+
+struct parsetree *newNode()
+{
+    struct parsetree *node = (struct parsetree *)malloc(sizeof(struct parsetree));
+    node->c = ' ';
+    node->left = NULL;
+    node->right = NULL;
+
+    return (node);
+}
+
+void printCurrentLevel(struct parsetree *root, int level, int h);
+int height(struct parsetree *node);
+
+/* Function to print level order traversal a tree*/
+void printLevelOrder(struct parsetree *root)
+{
+    int h = height(root);
+    int i;
+    for (i = 1; i <= h; i++)
+    {
+        for (int j = 0; j < pow(2, h - 3) - i; j++)
+            printf("  ");
+        printCurrentLevel(root, i, h - i);
+        printf("\n\n");
+    }
+}
+
+/* Print nodes at a current level */
+void printCurrentLevel(struct parsetree *root, int level, int h)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    if (level == 1)
+    {
+
+        printf("%c", root->c);
+    }
+    else if (level > 1)
+    {
+        printCurrentLevel(root->left, level - 1, h);
+        printf("   ");
+        printCurrentLevel(root->right, level - 1, h);
+    }
+}
+
+
+int height(struct parsetree *node)
+{
+    if (node == NULL)
+        return 0;
+    else
+    {
+        int lheight = height(node->left);
+        int rheight = height(node->right);
+
+        if (lheight > rheight)
+            return (lheight + 1);
+        else
+            return (rheight + 1);
+    }
+}
+
+int main()
+{
+    char input[20];
+    printf("Enter the Binary String:");
+    scanf("%s", input);
+    struct parsetree *head = newNode();
+    struct parsetree *child1 = newNode();
+    struct parsetree *child2 = newNode();
+    head->c = 'S';
+    head->left = child1;
+    head->right = child2;
+    child1->c = 'l';
+    child2->c = 'l';
+    int count = 0;
+    for (int i = 0; i < strlen(input); i++)
+    {
+        if (input[i] == '.')
+        {
+            break;
+        }
+        count++;
+    }
+    struct parsetree *temp = head->left;
+    for (int i = count - 1; i >= 0; i--)
+    {
+        if (i > 0)
+        {
+            struct parsetree *c1 = newNode();
+            struct parsetree *c2 = newNode();
+            temp->left = c1;
+            temp->right = c2;
+            struct parsetree *value = newNode();
+            c2->c = 'b';
+            c2->right = NULL;
+            c2->left = value;
+            value->c = input[i];
+            value->left = NULL;
+            value->right = NULL;
+            c1->c = 'l';
+            temp = c1;
+        }
+        else
+        {
+            temp->right = NULL;
+            struct parsetree *c2 = newNode();
+            struct parsetree *value = newNode();
+            temp->left = c2;
+            c2->c = 'b';
+            c2->left = value;
+            c2->right = NULL;
+            value->c = input[0];
+            value->left = NULL;
+            value->right = NULL;
+        }
+    }
+
+    temp = head->right;
+    int length = strlen(input);
+    for (int i = count + 1; i < length; i++)
+    {
+        if (i != length - 1)
+        {
+            struct parsetree *c1 = newNode();
+            struct parsetree *c2 = newNode();
+            temp->left = c2;
+            temp->right = c1;
+            struct parsetree *value = newNode();
+            c2->c = 'b';
+            c2->right = NULL;
+            c2->left = value;
+            value->c = input[i];
+            value->left = NULL;
+            value->right = NULL;
+            c1->c = 'l';
+            temp = c1;
+        }
+        else
+        {
+            temp->right = NULL;
+            struct parsetree *c2 = newNode();
+            struct parsetree *value = newNode();
+            temp->left = c2;
+            c2->c = 'b';
+            c2->left = value;
+            c2->right = NULL;
+            value->c = input[0];
+            value->left = NULL;
+            value->right = NULL;
+        }
+    }
+
+
+    printf("Level Order traversal of binary tree is \n");
+    printLevelOrder(head);
+
+    double result = 0;
+    for (int i = count; i > 0; i--)
+    {
+        if (input[count - i] == '1')
+        {
+            result += pow(2, i - 1);
+        }
+    }
+    for (int i = count + 1; i < length; i++)
+    {
+        if (input[i] == '1')
+        {
+            result += pow(2, count - i);
+        }
+    }
+    printf("\nResult: %lf\n", result);
+
+    return 0;
+}
