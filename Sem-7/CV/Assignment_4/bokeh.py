2c642b3fa8233e60d513050bb4f94ce2fcc3086a
NSVEGUR <cs20b1016@iiitdm.ac.in>
2023-11-04 21:58:43 +0530

    update: added bokeh effect using gradio

--- Sem-7/CV/Assignment_4/bokeh.py
+++ Sem-7/CV/Assignment_4/bokeh.py
@@ -1,27 +1,29 @@
+import gradio as gr
 import cv2
-import numpy as np
-from flask import Flask, request, jsonify, send_file
-import matplotlib.pyplot as plt
-from io import BytesIO
-import os
 
-app = Flask(__name__)
+
 
 def determine_parameters(input_image):
     # Convert the input image to grayscale
     grayscale_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2GRAY)
 
     # Apply adaptive thresholding to determine the optimal threshold value
-    threshold_value,tup = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    threshold_value, tup = cv2.threshold(grayscale_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
     # Determine the optimal Gaussian blur kernel size and standard deviation based on the image size
     height, width = grayscale_image.shape
+    # Ensure kernel size is odd and at least 3
     kernel_size = (max(3, int(width / 10) | 1), max(3, int(height / 10) | 1))
-    sigma = (kernel_size[1] * 0.5 - 1) * 0.2
+    sigma = (kernel_size[1]*0.5 - 1)*0.2
 
     return threshold_value, kernel_size, sigma
 
-def segment_and_blur_background(input_image):
+def apply_bokeh_effect(input_image):
+    # Check if the input image is loaded correctly
+    if input_image is None:
+        print("Error: Check if the input image path is correct")
+        return
+
     # Determine parameters based on the input image
     threshold_value, kernel_size, sigma = determine_parameters(input_image)
 
@@ -42,21 +44,5 @@ def segment_and_blur_background(input_image):
 
     return input_image
 
-@app.route('/segment_and_blur', methods=['POST'])
-def segment_and_blur():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image provided'})
-
-    image = request.files['image'].read()
-    image = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
-
-    blurred_image = segment_and_blur_background(image)
-
-    # Save the blurred image to a temporary file
-    output_filename = 'blurred_output.jpg'
-    cv2.imwrite(output_filename, blurred_image)
-
-    return send_file(output_filename, mimetype='image/jpeg')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+demo = gr.Interface(apply_bokeh_effect, gr.Image(), "image", css="body {background-color: #f2f2f2;}")
+demo.launch()
